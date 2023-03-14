53db5397a00b3c7db23db601c0f43d53be18ba52
ergg <18083548+ergg@users.noreply.github.com>
2023-03-14 12:25:11 +0100

    adding css static conf

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_stylesheet("css/custome.css")
+# html_static_path = ['_static']
 # html_css_files = ['css/custom.css']
 # html_logo = '_static/logo.png'
 # html_favicon = "_static/favicon.ico"
